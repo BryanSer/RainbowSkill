c686067ff9d7bfd9db7e165db579b8104b55e0dd
BryanSer <bryan_lzh@njust.edu.cn>
2019-12-26 20:51:08 +0800

    技能类

--- src/main/java/com/github/bryanser/rainbowskill/Skill.kt
+++ src/main/java/com/github/bryanser/rainbowskill/Skill.kt
@@ -1,6 +1,24 @@
 package com.github.bryanser.rainbowskill
 
+import com.relatev.minecraft.RainbowHero.skill.CastResultType
+import com.relatev.minecraft.RainbowHero.skill.Castable
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import java.util.*
+
 abstract class Skill(
-        val name: String
-) {
+        val name: String,
+        internal val description: MutableList<String>,
+        internal val displayMaterial: Material
+) : Castable {
+    abstract fun onCast(player: Player, level: Int): EnumMap<CastResultType, Any>
+
+    override fun cast(player: Player, level: Int): EnumMap<CastResultType, Any> {
+        TODO()
+        return onCast(player, level)
+    }
+
+    override fun getDescription(): MutableList<String> = description
+
+    override fun getDisplayMaterial(): Material = displayMaterial
 }
